369d514cf51d06ddc8419c09a31620b44462b667
Hari Gopal <mail@harigopal.in>
2021-10-20 23:34:48 +0530

    Update report format and test

---
+++ README.md
@@ -0,0 +1,32 @@
+# Solutions & tests for WD201 assignmets
+
+## Format for `report.json`
+
+```json
+{
+  "version": 0,
+  "grade": "skip/accept/reject",
+  "status": "success/failure",
+  "feedback": [
+    {
+      "key": "key_1_for_feedback_text",
+      "variables": {
+        "text_variable_key_1": "text_variable_value_1",
+        "text_variable_key_2": "text_variable_value_2"
+      }
+    },
+    {
+      "key": "key_2_for_feedback_text"
+    },
+  ],
+  "report": [
+    {
+      "key": "key_1_for_report_text",
+      "variables": {
+        "text_variable_key_1": "text_variable_value_1",
+        "text_variable_key_2": "text_variable_value_2"
+      }
+    }
+  ]
+}
+```
--- l2/test.rb
+++ l2/test.rb
@@ -3,37 +3,45 @@ require 'open3'
 
 @report = {
   version: 0,
-  reject: false,
-  accept: false,
-  communication: [],
+  grade: :skip,
+  status: :failure,
+  feedback: [],
+  report: []
 }
 
-def test(command, matcher:, comm_key:, comm_type: :feedback, reject: true)
+def test(command, include:, exclude: false, comm_key:, comm_type: :feedback, grade: :reject)
   stdout, stderr, status = Open3.capture3(command)
 
   if status != 0
-    @report[:reject] = reject
-    @report[:communication] << { type: comm_type, key: comm_key, locals: { response: stderr.strip } }
-  elsif !stdout.match?(matcher)
-    @report[:reject] = reject
-    @report[:communication] << { type: comm_type, key: comm_key, locals: { response: stdout.strip } }
+    @report[:grade] = grade
+    @report[comm_type] << { key: comm_key, variables: { response: stderr.strip } }
+  elsif exclude && stdout.match?(exclude)
+    @report[:grade] = grade
+    @report[comm_type] << { key: comm_key, variables: { response: stdout.strip } }
+  elsif !stdout.match?(include)
+    @report[:grade] = grade
+    @report[comm_type] << { key: comm_key, variables: { response: stdout.strip } }
   end
 end
 
 # Check a domain with an A record.
-test('ruby lookup.rb ruby-lang.org', matcher: /ruby-lang.org\s*=>\s*221.186.184.75/, comm_key: 'a_record_fail')
+test('ruby lookup.rb ruby-lang.org', include: /ruby-lang.org\s*=>\s*221.186.184.75/, exclude: /error/i, comm_key: 'a_record_fail')
 
 # Check a domain with a CNAME.
-test('ruby lookup.rb gmail.com', matcher: /gmail.com\s*=>\s*mail.google.com\s*=>\s*google.com\s*=>\s*172.217.163.46/, comm_key: 'cname_record_fail')
+test('ruby lookup.rb gmail.com', include: /gmail.com\s*=>\s*mail.google.com\s*=>\s*google.com\s*=>\s*172.217.163.46/, exclude: /error/i, comm_key: 'cname_record_fail')
 
 # Check a domain that isn't present in the zone file.
-test('ruby lookup.rb foo.com', matcher: /error\S?\s*record\s*not found.*foo.com/i, comm_key: 'missing_record_fail')
+test('ruby lookup.rb foo.com', include: /error\S?\s*record\s*not found.*foo.com/i, comm_key: 'missing_record_fail')
 
-unless @report[:reject]
-  @report[:communication] << { type: :report, key: 'critical_tests_passed' }
+if @report[:grade] == :skip
+  @report[:report] << { key: 'critical_tests_passed' }
+
+  # Check a CNAME that isn't linked to an A record.
+  test('ruby lookup.rb nowhere.example.com', include: /error\S?\s*record\s*not found.*example.com/i, comm_key: 'unlinked_record_fail', comm_type: :report, grade: :skip)
+
+  if @report[:report].length == 1
+    @report[:status] = :success
+  end
 end
-
-# Check a CNAME that isn't linked to an A record.
-test('ruby lookup.rb nowhere.example.com', matcher: /error\S?\s*record\s*not found.*example.com/i, comm_key: 'unlinked_record_fail', comm_type: :report, reject: false)
 
 File.write('report.json', JSON.pretty_generate(@report) + "\n")
